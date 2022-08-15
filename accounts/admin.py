d8061ff831f3b1458585caddfe42a7fce3a247d4
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-08-15 18:35:58 +0300

    update: create costume user modle

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+# from .forms import CustomUserCreationForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+        ("Personal Info", {
+            'fields': ('first_name', 'last_name', 'phone_number')
+        })
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
